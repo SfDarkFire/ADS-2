5e7e387cee5a286322c24e5419a9a666abfc8064
SfDarkFire <160343903+SfDarkFire@users.noreply.github.com>
2024-06-07 19:28:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,7 +26,7 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
+  double sum = 1.0;
   double term = 1.0;
   for (uint16_t i = 0; i < count; ++i) {
     sum += term;
